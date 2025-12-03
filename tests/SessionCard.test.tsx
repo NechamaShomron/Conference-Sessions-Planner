d62e511b91a402ffa757a02fa2e790f6cf0b6613
Nechama Shomron <nechamasho@gmail.com>
2025-12-03 20:47:47 +0200

    fixed test

--- tests/SessionCard.test.tsx
+++ tests/SessionCard.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import SessionFullDetails from "../components/session/SessionFullDetails";
+import SessionInfo from "../components/Shared/SessionInfo";
 import { getSessions } from "../lib/sessions";
 import "@testing-library/jest-dom";
 
@@ -7,7 +7,7 @@ describe("SessionCard", () => {
   it("renders a session's title, speaker, and room", () => {
     const sessions = getSessions();
     const session = sessions[0];
-    render(<SessionFullDetails session={session} />);
+    render(<SessionInfo session={session} showTitle={true} />);
 
     expect(screen.getByText(session.title)).toBeInTheDocument();
     expect(screen.getByText(session.room)).toBeInTheDocument();
